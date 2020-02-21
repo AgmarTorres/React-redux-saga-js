85f9a1f15f79d09e816f211caab82f250d31b3a9
Agmar Torres <agmartorres@gmail.com>
2020-02-21 18:00:45 -0300

    adding redux thunk

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -1,7 +1,8 @@
 import { ADD_ARTICLE } from "../constants/action-types";
 
 const initialState = {
-  articles: [{id:1, title:1}]
+  articles: [{id:1, title:1}],
+  remoteArticles: []
 };
 
 function rootReducer(state = initialState, action) {
@@ -10,7 +11,14 @@ function rootReducer(state = initialState, action) {
         articles: state.articles.concat(action.payload)
       });
     }
+  
+    if (action.type === "DATA_LOADED") {
+      return Object.assign({}, state, {
+        remoteArticles: state.remoteArticles.concat(action.payload)
+      });
+    }
     return state;
-}
+  
+  }
 
 export default rootReducer;
