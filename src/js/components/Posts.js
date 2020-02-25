daa2f602eaed65d40a863285e0aa435f37c52b01
Agmar Torres <agmartorres@gmail.com>
2020-02-24 21:49:26 -0300

    updating Posts file

--- src/js/components/Posts.js
+++ src/js/components/Posts.js
@@ -8,27 +8,16 @@ export class Post extends Component {
   }
 
   componentDidMount() {
+    // calling the new action creator
     this.props.getData();
   }
 
   render() {
-    return (
-      <ul>
-        {this.props.articles.map(el => (
-          <li key={el.id}>{el.title}</li>
-        ))}
-      </ul>
-    );
+    return null;
   }
 }
 
-function mapStateToProps(state) {
-  return {
-    articles: state.remoteArticles.slice(0, 10)
-  };
-}
-
 export default connect(
-  mapStateToProps,
+  null,
   { getData }
 )(Post);
